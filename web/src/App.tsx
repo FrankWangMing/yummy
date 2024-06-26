1ab8b1f232ccd62bcec31f401edb325bd623ca3a
Frank Wang <jasonming1998@gmail.com>
2024-06-26 19:24:57 +0800

    init

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 
 import './App.css'
+import Peer from "peerjs";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+    const  peer = new Peer();
+    peer.on('open', function(id) {
+        console.log('My peer ID is: ' + id);
+    });
   return (
     <>
-      <h1 class={"text-2xl"}>hello</h1>
+      <h1 className={"text-2xl"}>hello</h1>
     </>
   )
 }
